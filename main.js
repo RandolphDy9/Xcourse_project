07bbf255f8f4b0b13a88d8fc02c0589308c9c933
RandolphDy9 <mayson.dy@gmail.com>
2018-06-27 11:02:56 +0800

    A more responsive site with a registration page.

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
 
-// var btn = document.getElementById('begin');
-
-// btn.addEventListener("click", function(){
-//     alert('tadaaaa!');
-// });
-
 $(document).ready(function(){
-    // $("#begin").click(function(){
-    //     alert("I used jQuery for this!");
-    // });
-
-
-    // $("#tutors").on("mouseover", function(){
-    //     $(".tutor").toggle();
-    // });
-
-    // $(window).resize(function(){
-        if($(window).width()<576){
-            $(".hasRow").removeClass('row');
+    var i = 1;
+    $(".nav-pic").on("mouseover", function(){
+        if (i%2 == 1) {
+            $(".nav-item").hide();
+            $(this).css({opacity: 1})
         } else {
-            $(".hasRow").addClass('row');
+            $(".nav-item").show();
+            $(this).css({opacity: .7})
         }
-    // });
+        i++;
+    });
+
+    $("#genderForm input").on('click', function(){
+        if ($('input[name=gridRadios]:checked', '#genderForm').val() == "male") {
+            // alert("you selected a male!");
+            $("#maleOrFemale").attr('src', 'img/boy.png');
+        } else {
+            // alert("you selected a female!");
+            $("#maleOrFemale").attr('src', 'img/girl.png');
+        }
+    });
 
 });
